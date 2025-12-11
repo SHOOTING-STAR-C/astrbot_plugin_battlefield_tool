8e90203faa6d9d290ffd6488942becbbde2a0651
SHOOTING_STAR_C <944153243@qq.com>
2025-12-11 19:31:06 +0800

    release:v2.1.4

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import aiohttp
     "astrbot_plugin_battlefield_tool",  # name
     "SHOOTING_STAR_C",  # author
     "战地风云战绩查询插件",  # desc
-    "v2.1.3",  # version
+    "v2.1.4",  # version
 )
 class BattlefieldTool(Star):
 
